edf3aae744e333878438f00673a9aac0bb6b16f0
martin obodo <martinobodo99@gmail.com>
2024-12-30 10:35:07 +0000

    updated settings.py to debug = false

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$cetv#hc@h7b*ws(*bzc)89-&4l4l&30n4w@s@b2d@3+c4d^d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

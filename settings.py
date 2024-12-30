e709d1bf7b7e549c37f94304521965c42a1315d4
martin obodo <martinobodo99@gmail.com>
2024-12-30 10:33:40 +0000

    added procfile

--- settings.py
+++ settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# Static Root 
 
 STATIC_URL = 'static/'
 
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
